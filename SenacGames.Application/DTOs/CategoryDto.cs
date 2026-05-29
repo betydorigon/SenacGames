6281918a3861d82b2be0797c051fed8a229a68ad
Bety <betydorigon@gmail.com>
2026-05-29 14:42:33 -0300

    Primeiro Commit SenacGames

---
+++ SenacGames.Application/DTOs/CategoryDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenacGames.Application.DTOs
+{
+    public class CategoryDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public int GameCount { get; set; }
+    }
+
+    public class CreateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty ;
+
+    }
+
+    public class UpdateCategoryDto
+    {
+        public string Name { get; set; } = string.Empty;
+    }
+}
---
+++ SenacGames.Application/DTOs/GameDto.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SenacGames.Application.DTOs
+{
+    public class GameDto
+    {
+        public int id { get; set; }
+        public string Title { get; set; } = string.Empty;
+
+        public string Description { get; set; } = string.Empty;
+
+        public int ReleaseYear { get; set; }
+        public string coverImageUrl { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+
+        public bool isFeatured { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+
+
+    public class CreateGameDto
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public string CoverImageUrl { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+        public bool IsFeatured { get; set; }
+    }
+    public class UpdateGameDto
+    {
+        public string Title { get; set; } = String.Empty;
+        public string Description { get; set; } = string.Empty;
+        public int ReleaseYear { get; set; }
+        public string CoverImageUrl { get; set; } = string.Empty;
+
+        public int CategoryId { get; set; }
+        public bool IsFeatured { get; set; }
+
+    }
+}
